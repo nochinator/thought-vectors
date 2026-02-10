5ffe35c15519999ec36d32b5fd017603b29791dc
nochinator <3nochinator@proton.me>
2026-02-10 08:32:36 -0500

    local updates

--- scripts/train_with_preset.sh
+++ scripts/train_with_preset.sh
@@ -7,15 +7,15 @@ SNLI="../datasets/SNLI_train.csv"
 MINIPILE="../datasets/minipile.csv"
 C41="../datasets/C4subset-1.csv"
 C42="../datasets/C4subset-2.csv"
-OUTPUT_PATH="artifacts/thought_vectors0.1-1.pt"
+OUTPUT_PATH="artifacts/thought_vectors0.1-3.pt"
 MODEL_PATH="artifacts/thought_vectors0.1-2.pt"
 
 python train_model.py \
-  --data "$SNLI" \
-  --tokenizer_texts "$STSB" "$SNLI" "$MINIPILE" "$C41" "$C42" \
+  --data "$MINIPILE" \
   --resume-from "$MODEL_PATH" \
+  --tokenizer-count-memory-limit 24 \
   --epochs 1 \
-  --batch-size 128 \
+  --batch-size 64 \
   --lr 1e-4 \
   --weight-decay 1e-5 \
   --length-penalty 0.01 \
